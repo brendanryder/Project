6720ea24592c57706b4d07ddb009e09d0618fedf
Brendan Ryder <brendan-ryder@hotmail.com>
2018-03-07 14:59:10 +0000

    Exported css to a .css file. User can use either username or email to login. Password chech updated tolook for md5 hashed password stored in database. Minor aesthetic freatures changed

--- WebLoginPage/login.php
+++ WebLoginPage/login.php
@@ -1,59 +1,66 @@
+
+
 <?php
    include("config.php");
    session_start();
    $error = false;
+   $myusername = '';
+   $mypassword = '';
+   //$error = "Your Login Name or Password is invalid";
    
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
       
-      $user = mysqli_real_escape_string($db,$_POST['username']);
-      $pass = mysqli_real_escape_string($db,$_POST['password']); 
-      
-      $sql = "SELECT id FROM admin WHERE username = '$user' AND BINARY passcode = '$pass'";
+      $myusername = $_POST['username'];
+      $mypassword = $_POST['password']; 
+   
+	  $sql = "SELECT * FROM admin WHERE (email = '$myusername' OR user = '$myusername') AND pass = '" . md5($mypassword). "'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
       
       $count = mysqli_num_rows($result);
       
-      // If result matched $user and $pass, table row must be 1 row
+      // If result matched $myusername and $mypassword, table row must be 1 row
 		
       if($count == 1) {
-         $_SESSION['login_user'] = $user;
+         $_SESSION['login_user'] = $myusername;
          
-         header("location: weather.php");
+         header("location: index.php");
 		 
       }else {
-          $error = "Incorrect Username or Password";
+          $error = "Incorrect Username or Password Entered";
       }
    }
 ?>
 <html>
 <head>
+<link href="/css/c3.css" rel="stylesheet" type="text/css">
 <title>Weather Monitor Login</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
 </head>
 <body>
+<style>
+
+</style>
 
 <h2><center>Weather Monitor Login</center></h2>
 
-<form action = "" method = "post">
-  <div class="imgcontainer">
-    <img src="weather.png" alt="Avatar" class="avatar">
+<form class="loginForm" action = "" method = "post">
+  <div class="LoginImgContainer">
+    <img class = "LoginImg" src="weather.png" alt="weather">
   </div>
 
-  <div class="container">
-    <label><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="username" required>
-    <label><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="password" required>
-        
-    <button type="submit">Login</button>
+  <div class="LoginContainer">
+    <input class="LoginInput" type="text" placeholder="Email or Username" name="username" required>
+    <input class="LoginInput" type="password" placeholder="Password" name="password" required>
+
+    <button class="loginBtn" type="submit">Login</button>
+	<a href="register.php">Register</a>
   </div>
 
 </form>
-<center><div style = "font-size:25px; color:#cc0000; margin-top:100px"><?php echo @$error; ?></div>
+<center><div style = "font-size:25px; color:#cc0000; margin-top:100px"><?php echo $error; ?></div>
 
 </body>
 </html>
