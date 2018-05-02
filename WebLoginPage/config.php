83ef0dc638c361a679f2c6bddec49aff84be2811
Brendan Ryder <brendan-ryder@hotmail.com>
2018-05-02 15:06:20 +0100

    Updated Database address and database name

--- WebLoginPage/config.php
+++ WebLoginPage/config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_address', '192.168.0.157');
+   define('DB_address', '192.168.43.79');
    define('DB_user', 'root');
    define('DB_pass', 'root');
-   define('DB_databaseName', 'mydb123456');
+   define('DB_databaseName', 'userLogin');
    $db = mysqli_connect(DB_address,DB_user,DB_pass,DB_databaseName);
 ?>
